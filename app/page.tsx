5dc73e3500e3d79c526922f4a9b77d13bf0f50e9
Jonathan_Tinti <jtintinti@gmail.com>
2026-05-29 13:34:44 -0400

    added styling to the page

--- app/page.tsx
+++ app/page.tsx
@@ -35,25 +35,64 @@ export default function LoginPage() {
     }
 
     return (
-        <div>
+        <div style={styles.container}>
+            <h2 style={styles.title}>
+                Welcome to Sports League Planner!
+            </h2>
             <input 
+            style={styles.input}
             type="email"
             placeholder="Email"
             value={email}
             onChange={(e) => setEmail(e.target.value)}
             />
             <input 
+            style={styles.input}
             type="password"
             placeholder="Password"
             value={password}
             onChange={(e) => setPassword(e.target.value)}
             />
-            <button onClick={signIn}>
+            <button onClick={signIn} style={styles.button}>
                 Login
             </button>
-            <button onClick={signUp}>
+            <button onClick={signUp} style={styles.button}>
                 Not Registered? Sign Up!
             </button>
         </div>
     )
 }
+
+const styles = {
+    container: {
+        backgroundColor: '#bfdde2',
+        display: 'flex', 
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+        height: '100vh',
+        width: '100vw',
+    },
+    input: {
+        border: '1px solid', 
+        borderColor: '#000000', 
+        padding: '20px', 
+        margin: '10px', 
+        borderRadius: '5px',
+    }, 
+    button: {
+        backgroundColor: '#96d2fa',
+        border: '1px solid', 
+        borderColor: '#000000',
+        padding: '10px 20px',
+        cursor: 'pointer',
+        margin: '5px', 
+        borderRadius: '5px',
+    },
+    title: {
+        fontSize: '28px',
+        fontWeight: 'bold',
+        position: 'sticky',
+        top: 0
+    }
+} satisfies Record<string, React.CSSProperties>
